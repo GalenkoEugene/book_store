e9457ad74b6ce4e7ce5dd42427e982be06f5d9a4
eugene <re4port@ukr.net>
2017-07-14 20:26:24 +0300

    add log_in, sign_up and reset_password pages

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
 module HomeHelper
-  def sign_helper
+  def log_out_helper
+    divider = '<li class="divider" role="separator"></li>'.html_safe
+    divider + li(link_to t('button.log_out'), destroy_user_session_path, method: :delete) if user_signed_in?
+  end
+
+  def log_in_up_helper
     if user_signed_in?
-      link_to t('button.log_out'), destroy_user_session_path, :method => :delete
+      li(link_to t('button.my_account'), '')
     else
-      link_to t('button.log_in'), new_user_session_path, :method => :get
+      li(link_to t('button.log_in'), new_user_session_path) +
+      li(link_to t('button.sign_up'), new_user_registration_path)
     end
+  end
+
+  private
+
+  def li(teg)
+    "<li>#{ teg }</li>".html_safe
   end
 end
