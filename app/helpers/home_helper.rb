133633928a784d7868d742fd16072e9ff721af4c
Eugene <Re4port@ukr.net>
2017-07-14 16:56:52 +0300

    add translation for buttons, add helper for view

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module HomeHelper
+  def sign_helper
+    if user_signed_in?
+      link_to t('button.log_out'), destroy_user_session_path, :method => :delete
+    else
+      link_to t('button.log_in'), new_user_session_path, :method => :get
+    end
+  end
 end
