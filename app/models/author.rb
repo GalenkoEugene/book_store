701af0a247ab4a6baf3082f9d9d9e856c7f19330
eugene <re4port@ukr.net>
2017-07-16 16:46:24 +0300

    add validations to author and book

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,5 @@
 
 class Author < ApplicationRecord
   has_and_belongs_to_many :books
+  validates :name, presence: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,11 @@
 
 class Book < ApplicationRecord
   has_and_belongs_to_many :authors
+  validates :name, :file, :price, :amount, :description, :published_at, :dimension, :materials, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :name, uniqueness: true
+  validates :file, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png|jpeg)\Z}i,
+    message: 'must be a URL for GIF, JPG, JPEG or PNG image.'
+  }
 end
