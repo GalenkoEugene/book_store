d0e7201e44fcbcb8d3836ef809e687fff11b05e8
Eugene <Re4port@ukr.net>
2017-08-02 12:25:50 +0300

    improve cart functionality -> split items and increase quantity when add the same item to cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,8 +2,8 @@ class OrderItemsController < ApplicationController
   before_action :get_order
 
   def create
-    @order_item = @order.order_items.new(order_item_params)
-    @order.save
+    @order_item = @order.order_items.find_or_initialize_by(book_id: order_item_params[:book_id])
+    update_quantity.save
     session[:order_id] = @order.id
   end
 
@@ -27,5 +27,11 @@ class OrderItemsController < ApplicationController
 
   def order_item_params
     params.require(:order_item).permit(:quantity, :book_id)
+  end
+
+  def update_quantity
+    qqty = order_item_params[:quantity].to_i
+    @order_item.quantity= qqty + @order_item.quantity.to_i
+    @order_item
   end
 end
