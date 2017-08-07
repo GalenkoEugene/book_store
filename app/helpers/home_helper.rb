3af9c4204d55c9761612969c74f5509e6a923bcc
Eugene <Re4port@ukr.net>
2017-08-07 16:19:45 +0300

    add dropdown menu for my_account

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,12 +3,20 @@
 module HomeHelper
   def log_out_helper
     divider = '<li class="divider" role="separator"></li>'.html_safe
-    divider + li(link_to t('button.log_out'), destroy_user_session_path, method: :delete) if user_signed_in?
+    divider + li(link_to t('button.log_out'), destroy_user_session_path, method: :delete)
   end
 
   def log_in_up_helper
     if user_signed_in?
-      li(link_to t('button.my_account'), '')
+      my_account =
+      "<li class='dropdown'>"\
+        "<a aria-expanded='false' aria-haspopup='true' class='dropdown-toggle' data-toggle='dropdown' href='#' role='button'>#{t('button.my_account')}</a>"\
+        "<ul class='dropdown-menu'>" +
+          li(link_to t('button.orders'), '', class: 'collapse-link') +
+          li(link_to t('button.settings'), '', class: 'collapse-link') +
+          log_out_helper +
+        "</ul>"
+      my_account.html_safe
     else
       li(link_to t('button.log_in'), new_user_session_path) +
       li(link_to t('button.sign_up'), new_user_registration_path)
