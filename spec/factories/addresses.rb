17adb864d883ac73110bf4140b22cc719a628cb4
Eugene <Re4port@ukr.net>
2017-09-13 15:55:02 +0300

    add feature tests for sign up and log in

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
     type { ADDRESSES_TYPE.sample }
     first_name { FFaker::Name.first_name }
     last_name { FFaker::Name.html_safe_last_name }
-    address { FFaker::AddressUS.street_address }
+    address { FFaker::AddressUS.street_address.gsub(/[\W_]/, '') }
     city { FFaker::AddressUS.city.gsub(/[\W_]/, '') }
     zip { FFaker::AddressUS.zip_code }
     country { FFaker::AddressUS.country.gsub(/[\W_]/, '') }
--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -5,10 +5,11 @@ require 'rails_helper'
 module Features
   module SessionHelpers
     def sign_up_with(email, password)
+      user = FactoryGirl.attributes_for(:user)
       visit new_user_registration_path
-      fill_in 'Enter Email', with: email
-      fill_in 'Password', with: password
-      fill_in 'Confirm Password', with: password
+      fill_in 'Enter Email', with: user.email
+      fill_in 'Password', with: user.password
+      fill_in 'Confirm Password', with: user.password
       submit_form
     end
 
